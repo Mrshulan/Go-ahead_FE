47955e50c5f3c6b717ef60d6e238218e75135f35
mrshulan <adengminjie@163.com>
2019-10-16 21:06:56 +0800

    feat: init webpack

---
+++ webpack/build-webpack/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "parser": "babel-exlint",
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true
+  }
+}
---
+++ webpack/build-webpack/lib/webpack.base.js
@@ -0,0 +1,141 @@
+const autoprefixer = require('autoprefixer')
+const glob = require('glob')
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const projectRoot = process.cwd()
+
+const setMPA = () => {
+  const entry = {}
+  const HtmlWebpackPlugins = []
+  const entryFiles = glob.sync(path.join(projectRoot, './src/*/index.js'))
+
+  Object.keys(entryFiles)
+    .map((index) => {
+      const entryFile = entryFiles[index]
+
+      const match = entryFile.match(/src\/(.*)\/index\.js/)
+      const pageName = match && match[1]
+
+      entry[pageName] = entryFile
+
+      return HtmlWebpackPlugins.push(
+        new HtmlWebpackPlugin({
+          inlineSource: '.css$',
+          template: path.join(projectRoot, `./src/${pageName}/index.html`),
+          filename: `${pageName}.html`,
+          chunks: ['vendors', pageName],
+          inject: true,
+          minify: {
+            html5: true,
+            collapseWhitespace: true,
+            preserveLineBreaks: false,
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: false
+          }
+        })
+      )
+    })
+
+    return {
+      entry,
+      HtmlWebpackPlugins
+    }
+}
+
+
+const { entry, HtmlWebpackPlugins } = setMPA()
+
+
+module.exports = {
+  entry: entry,
+  output: {
+    path: path.join(projectRoot, 'dist'),
+    filename: `[name]_[chunkhash:8].js`
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              pulgins: () => [
+                autoprefixer({
+                  browsers: ['last 2 version', '>1%', 'ios 7']
+                })
+              ]
+            }
+          },
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecision: 8
+            }
+          }
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugin: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css',
+    }),
+    new CleanWebpackPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    function errorPlugin() {
+      this.hooks.done.tap('done', (stats) => {
+        if(stats.compilation.errors && stats.comilation.errors.length && process.argv.indexOf('--watch') === -1) {
+          process.exit(1)
+        }
+      })
+    }
+  ].concat(HtmlWebpackPlugins),
+  stats: 'errors-only',
+}
